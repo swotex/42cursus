cb78fb4d5886b3ae923e63c7b333c8408b09ac30
swotex <swotex21@gmail.com>
2024-07-20 03:02:10 +0200

    s

--- arachnida/scorpion/graphics.py
+++ arachnida/scorpion/graphics.py
@@ -1,10 +1,12 @@
 import tkinter
 import customtkinter
 
-def __addImages(frame, allFiles):
-	for index, image in enumerate(allFiles, start=1):
-		button = customtkinter.CTkButton(frame, text=image.name[:10], command=None)
-		button.grid(row=index, column=0, padx=10, pady=5)
+def clear_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def __printImageInfo(image, mainFrame):
+	print(image.name)
 
 def createWindow(allFiles):
 	width = 850
@@ -18,11 +20,15 @@ def createWindow(allFiles):
 	sidebar_frame = customtkinter.CTkScrollableFrame(root, width=160, height=height, corner_radius=0)
 	sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
 
-	__addImages(sidebar_frame, allFiles)
 
 
 	entry = customtkinter.CTkFrame(root, width=(width - 160 - 40), height=(height - 40))
 	entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
 
+	for index, image in enumerate(allFiles, start=1):
+		button = customtkinter.CTkButton(sidebar_frame, text=image.name[:10], command=lambda img=image, frame=entry: __printImageInfo(img, frame))
+		button.grid(row=index, column=0, padx=10, pady=5)
+	
+	
 	root.mainloop()
