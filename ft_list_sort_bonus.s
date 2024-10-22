cdd5438d355f5f4cce74538c8aa526c452dfc36d
swotex <swotex21@gmail.com>
2024-10-22 08:20:43 +0200

    avancement list sort

--- ft_list_sort_bonus.s
+++ ft_list_sort_bonus.s
@@ -2,33 +2,58 @@ global ft_list_sort
 
 ; void ft_list_sort(t_list **begin_list, int (*cmp)()) rdi, rsi
 ft_list_sort:
-	mov rdx, rsi
-	; sub rsp, 16 ; begin and last
-	mov rcx, rdi
+	sub rsp, 16 ; save t_list beggin and func
+	mov QWORD [rsp], rdi ; save t_list
+	; mov QWORD [rsp + 8], rsi ; save func
+	mov r13, rsi ; save func
+	mov rdx, [rdi] ; unreference list in rdx
+
+	cmp rdx, 0 ; si list NULL
+	je exit_sorted
+	mov rbx, [rdx + 8]
+
+	cmp rbx, 0 ; si list +1 NULL
+	je exit_sorted
+
 	jmp loop
 
-	switch_data:
-		mov r10, qword [rdi]
-		mov rdi, qword [rsi]
-		mov rsi, r10
-		mov rdi, rcx
+	; rdx noeud 1 rbx noeud 2
+
+	swap:
+		mov r10, [rdx]
+		mov r12, [rbx]
+		mov [rdx], r12
+		mov [rbx], r10
+
+	set_start:
+		mov rdx, QWORD [rsp]
+		; mov rdx, [rdx]
+		mov rbx, [rdx + 8]
 
 	loop:
-		mov rsi, [rdi + 8]
-		cmp rsi, 0
-		je return_sorted
-		push rdi
-		push rsi
-		mov rdi, rdi
-		mov rsi, [rsi]
-		call rdx
-		pop rsi
-		pop rdi
-		cmp rax, 0
-		jg switch_data
-		mov rdi, rsi
-	jmp loop
+		cmp rbx, 0
+		je exit_sorted
 
-	return_sorted:
-		mov rdi, rcx
+		mov rdi, [rdx]
+		mov rsi, [rbx]
+		push rdx
+		push rbx
+		call r13
+		pop rbx
+		pop rdx
+		cmp rax, 0
+		jg swap
+
+		; next
+		next:
+			mov rdx, rbx
+			mov rbx, [rdx + 8]
+			jmp loop
+
+	exit_sorted:
+		mov rdi, [rsp]
+		; mov rsi, [rsp + 8]
+		mov rsi, r13
+		add rsp, 16
+		xor rax, rax
 		ret
--- main_bonus.c
+++ main_bonus.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct s_list
 {
@@ -16,6 +17,46 @@ extern void ft_list_push_front(t_list **begin_list, void *data);
 extern int ft_list_size(t_list *begin);
 extern void ft_list_sort(t_list **begin_list, int (*cmp)());
 
+void putnumber(int n) {
+    // Gestion des nombres négatifs
+    if (n < 0) {
+        write(1, "-", 1); // Affiche le signe moins
+        n = -n; // Rend le nombre positif
+    }
+    
+    // Gérer le nombre 0 séparément
+    if (n == 0) {
+        write(1, "0", 1);
+        return;
+    }
+
+    // Variable pour stocker les chiffres
+    char buffer[12]; // 11 caractères pour un int et 1 pour le '\0'
+    int i = 0;
+
+    // Extraire les chiffres et les stocker dans le buffer
+    while (n > 0) {
+        buffer[i++] = (n % 10) + '0'; // Obtenir le dernier chiffre
+        n /= 10; // Réduire le nombre
+    }
+
+    // Afficher les chiffres en ordre inverse
+    for (int j = i - 1; j >= 0; j--) {
+        write(1, &buffer[j], 1); // Utiliser write pour afficher chaque chiffre
+    }
+}
+
+int testee(void *str1, void *str2)
+{
+    write(1, str1, strlen(str1));
+    write(1, " | ", 3);
+    write(1, str2, strlen(str2));
+    int tt = strcmp((char *)str1, (char *)str2);
+    putnumber(tt);
+    write(1, "\n", 1);
+    return (tt);
+}
+
 int main(void)
 {
     int back = ft_atoi_base(" ---+--+7CE", "0123456789ABCDEF");
@@ -29,7 +70,7 @@ int main(void)
 
     printf("list size : %d\n", lst_size);
 
-    ft_list_sort(&test, &strcmp);
+    ft_list_sort(&test, &testee);
 
     t_list *lst = test;
     while(lst)
