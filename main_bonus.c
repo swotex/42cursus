65e0e977d37f8c3d5995ff04203382e7f7afb040
swotex <swotex21@gmail.com>
2025-03-11 11:36:30 +0100

    Clean tests and add more

--- main_bonus.c
+++ main_bonus.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#define RESET "\033[0m"
+#define TITLE "\033[3;32m"
+#define SUBTITLE "\033[3;35m"
+
 typedef struct s_list
 {
     void *data;
@@ -34,7 +38,7 @@ void printHeader(const char *str)
     {
         printf(" ");
     }
-    printf("\033[3;32m%s\033[0m",str);
+    printf(TITLE"%s"RESET,str);
     for(int i=0; i < ((len / 2) + len % 2); i++)
     {
         printf(" ");
@@ -44,15 +48,6 @@ void printHeader(const char *str)
     printf("########################################\n\n");
 }
 
-void	ft_free(void *ptr)
-{
-    // (void)ptr;
-	// if (ptr)
-		// free(ptr);
-    printf("oui : %s \n", (char *)ptr);
-    *(char *)ptr = 'X';
-    printf("oui : %s \n", (char *)ptr);
-}
 
 void print_list(t_list **lst)
 {
@@ -119,30 +114,58 @@ t_list *get_list(int size)
 void test_atoi_base()
 {
     int back;
-    printf("----- Test with NULL -----\n");
-    back = ft_atoi_base(NULL, "0123456789ABCDEF");
-    printf("Result : %d\n", back);
-    back = ft_atoi_base(" ---+--+7CE", NULL);
-    printf("Result : %d\n", back);
+    printf("---------- "SUBTITLE"Test with NULL"RESET" ----------\n");
+    back = ft_atoi_base(NULL, "0123456789ABCDEF"); // input NULL
+    printf("(NULL, '0123456789ABCDEF') -> %d\n", back);
+    back = ft_atoi_base(" ---+--+7CE", NULL); // Base NULL
+    printf("(' ---+--+7CE', NULL)      -> %d\n", back);
 
-    printf("\n----- Test multiple operands (Base 16) -----\n");
-    back = ft_atoi_base(" ---+--+7CE", "0123456789ABCDEF");
-    printf("Result (' ---+--+7CE') : %d\n", back);
+    printf("\n---------- "SUBTITLE"Test with bad input"RESET" ----------\n");
+    back = ft_atoi_base("", "0123456789"); // empty input
+    printf("('', '0123456789')             -> %d\n", back);
+    back = ft_atoi_base("       - 23", "0123456789"); // white-space after operand
+    printf("('       - 23', '0123456789')  -> %d\n", back);
+    back = ft_atoi_base("       -23RT", "0123456789"); // Out of Operand
+    printf("('       -23RT', '0123456789') -> %d\n", back);
 
-    printf("\n----- Test Base 2 -----\n");
-    back = ft_atoi_base("00100101010001", "01");
-    printf("Result ('00100101010001') : %d\n", back);
+    printf("\n---------- "SUBTITLE"Test with bad base"RESET" ----------\n");
+    back = ft_atoi_base("23", ""); // empty base
+    printf("('23', '')        -> %d\n", back);
+    back = ft_atoi_base("23", " 123456"); // White-space in base
+    printf("('23', ' 123456') -> %d\n", back);
+    back = ft_atoi_base("23", "1234526"); // Twice item in base
+    printf("('23', '1234526') -> %d\n", back);
+    back = ft_atoi_base("23", "-123456"); // Operand in base
+    printf("('23', '-123456') -> %d\n", back);
+
+    printf("\n---------- "SUBTITLE"Test multiple operands (Base 16)"RESET" ----------\n");
+    back = ft_atoi_base("23", "0123456789ABCDEF"); // Simple test
+    printf("('23', '0123456789ABCDEF')                    -> %d\n", back);
+    back = ft_atoi_base(" ---+--+7CE", "0123456789ABCDEF"); // test with operand (negative)
+    printf("(' ---+--+7CE', '0123456789ABCDEF')           -> %d\n", back);
+    back = ft_atoi_base("      ---+--+++-+AE80", "0123456789ABCDEF"); // test with operand (positive)
+    printf("('      ---+--+++-+AE80', '0123456789ABCDEF') -> %d\n", back);
+
+    printf("\n---------- "SUBTITLE"Test Base 2"RESET" ----------\n");
+    back = ft_atoi_base("1010", "01"); // Simple convert binary
+    printf("('1010', '01')           -> %d\n", back);
+    back = ft_atoi_base("00100101010001", "01"); // Other convert binary
+    printf("('00100101010001', '01') -> %d\n", back);
 }
 
 void test_push_front()
 {
     t_list *lst = NULL;
 
-    printf("----- Test with NULL list -----\n");
+    printf("---------- "SUBTITLE"Test with NULL list"RESET" ----------\n");
     ft_list_push_front(NULL, "test");
+    printf("(NULL, 'test') -> ");
+    print_list(&lst);
+    ft_list_push_front(&lst, NULL);
+    printf("(&lst, NULL)   -> ");
     print_list(&lst);
 
-    printf("\n----- Test fill list -----\n");
+    printf("\n---------- "SUBTITLE"Test fill list"RESET" ----------\n");
     ft_list_push_front(&lst, "bad");
     ft_list_push_front(&lst, "small");
     ft_list_push_front(&lst, "champion");
@@ -158,27 +181,27 @@ void test_lst_size()
     t_list *lst = NULL;
     int size;
 
-    printf("----- Test NULL -----\n");
+    printf("---------- "SUBTITLE"Test NULL"RESET" ----------\n");
     size = ft_list_size(lst);
-    printf("Size (0) : %d\n", size);
+    printf("(NULL)         -> %d\n", size);
     free_list(&lst);
 
-    printf("\n----- Test size 5 -----\n");
+    printf("\n---------- "SUBTITLE"Test size 5"RESET" ----------\n");
     lst = get_list(5);
     size = ft_list_size(lst);
-    printf("Size (5) : %d\n", size);
+    printf("(list[size 5]) -> %d\n", size);
     free_list(&lst);
 
-    printf("\n----- Test size 3 -----\n");
+    printf("\n---------- "SUBTITLE"Test size 3"RESET" ----------\n");
     lst = get_list(3);
     size = ft_list_size(lst);
-    printf("Size (3) : %d\n", size);
+    printf("(list[size 3]) -> %d\n", size);
     free_list(&lst);
 
-    printf("\n----- Test size 1 -----\n");
+    printf("\n---------- "SUBTITLE"Test size 1"RESET" ----------\n");
     lst = get_list(1);
     size = ft_list_size(lst);
-    printf("Size (1) : %d\n", size);
+    printf("(list[size 1]) -> %d\n", size);
 
     free_list(&lst);
 }
@@ -187,27 +210,32 @@ void test_lst_sort()
 {
     t_list *lst = NULL;
 
-    printf("----- Test NULL -----\n");
+    printf("---------- "SUBTITLE"Test NULL"RESET" ----------\n");
     ft_list_sort(NULL, strcmp);
-    ft_list_sort(&lst, strcmp);
+    printf("(NULL, strcmp) -> NO ERROR\n");
+    ft_list_sort(&lst, NULL);
+    printf("(&lst, NULL)   -> NO ERROR\n");
 
-    printf("\n----- Test list size 5 -----\n");
+    printf("\n---------- "SUBTITLE"Test list size 5"RESET" ----------\n");
     lst = get_list(5);
+    printf("(list[size 5]) : bad, small, champion, reputation, frequency\n");
     ft_list_sort(&lst, strcmp);
     print_list(&lst);
     free_list(&lst);
 
-    printf("\n----- Test list size 1 -----\n");
+    printf("\n---------- "SUBTITLE"Test list size 1"RESET" ----------\n");
     lst = get_list(1);
+    printf("(list[size 1]) : bad\n");
     ft_list_sort(&lst, strcmp);
     print_list(&lst);
     free_list(&lst);
 
-    printf("\n----- Test list already sorted -----\n");
+    printf("\n---------- "SUBTITLE"Test list already sorted"RESET" ----------\n");
     lst = NULL;
     ft_list_push_front(&lst, "aaasdf");
     ft_list_push_front(&lst, "caasdf");
     ft_list_push_front(&lst, "haasdf");
+    printf("(list[size 3]) : aaasdf, caasdf, haasdf\n");
     ft_list_sort(&lst, strcmp);
     print_list(&lst);
     free_list(&lst);
@@ -218,62 +246,88 @@ void test_lst_remove()
 {
     t_list *lst = NULL;
 
-    // printf("----- Test with NULL -----\n");
-    // ft_list_remove_if(NULL, "test", strcmp, ft_free);
-    // ft_list_remove_if(&lst, "test", strcmp, ft_free);
+    printf("\n---------- "SUBTITLE"Test with NULL"RESET" ----------\n");
+    printf("Test with NULL for -> **begin_list, *begin_list, data_ref, compare function, free function\n");
+    ft_list_remove_if(NULL, "test", strcmp, free); // Null for **begin_list
+    ft_list_remove_if(&lst, "test", strcmp, free); // Null for *begin_list
+    ft_list_remove_if(&lst, NULL, strcmp, free); // Null for data_ref
+    ft_list_remove_if(&lst, "test", NULL, free); // Null for compare function
+    ft_list_remove_if(&lst, "test", strcmp, NULL); // Null for free function
 
-    // ft_list_push_front(&lst, "test");
+    printf("\n---------- "SUBTITLE"Test remove size 1 top"RESET" ----------\n");
+    ft_list_push_front(&lst, strdup("Hey"));
+    ft_list_remove_if(&lst, "Hey", ft_strcmp, free);
+    printf("(list[size 1], remove('Hey')) : Hey -> ");
+    print_list(&lst);
 
-    // ft_list_remove_if(NULL, "test2", NULL, ft_free);
-    // ft_list_remove_if(&lst, "test", strcmp, NULL);
+    printf("\n---------- "SUBTITLE"Test remove size 2 top"RESET" ----------\n");
+    ft_list_push_front(&lst, strdup("Hey"));
+    ft_list_push_front(&lst, strdup("Hey"));
+    ft_list_remove_if(&lst, "Hey", ft_strcmp, free);
+    printf("(list[size 1], remove('Hey')) : Hey, Hey -> ");
+    print_list(&lst);
 
-    // printf("\n----- Test remove 1 top -----\n");
-    // ft_list_remove_if(&lst, "test", strcmp, ft_free);
-    // ft_list_remove_if(&lst, "test", strcmp, free);
-
-    // ft_list_push_front(&lst, "test");
-    // ft_list_push_front(&lst, "test");
-
-    // printf("\n----- Test remove 2 top -----\n");
-    // ft_list_remove_if(&lst, "test", strcmp, ft_free);
-
-
-    // ft_list_push_front(&lst, "sdf4");
-    // ft_list_push_front(&lst, "sfsdf3");
-    // ft_list_push_front(&lst, "test2");
-    // ft_list_push_front(&lst, "test2");
-    // ft_list_push_front(&lst, "tesfsdfst1");
-
-    ft_list_push_front(&lst, strdup("milk"));
+    printf("\n---------- "SUBTITLE"Test middle remove"RESET" ----------\n");
+    ft_list_push_front(&lst, strdup("banana"));
     ft_list_push_front(&lst, strdup("bread"));
     ft_list_push_front(&lst, strdup("milk"));
-    ft_list_push_front(&lst, strdup("apple"));
+    ft_list_push_front(&lst, strdup("milk"));
     ft_list_push_front(&lst, strdup("ZEBRA"));
     ft_list_push_front(&lst, strdup("tomato"));
-    ft_list_push_front(&lst, strdup("milk"));
+    ft_list_push_front(&lst, strdup("other"));
 
-    printf("\n----- Test middle remove -----\n");
-    // ft_list_remove_if(&lst, "test2", strcmp, ft_free);
     ft_list_remove_if(&lst, "milk", strcmp, free);
+    printf("(list[size 7], remove('milk')) : other, tomato, ZEBRA, milk, milk, bread, banana\n");
     print_list(&lst);
 
     double_free_list(&lst);
-    // free_list(&lst);
+    lst = NULL;
+
+    printf("\n---------- "SUBTITLE"Test end remove"RESET" ----------\n");
+    ft_list_push_front(&lst, strdup("banana"));
+    ft_list_push_front(&lst, strdup("banana"));
+    ft_list_push_front(&lst, strdup("milk"));
+    ft_list_push_front(&lst, strdup("milk"));
+    ft_list_push_front(&lst, strdup("ZEBRA"));
+    ft_list_push_front(&lst, strdup("tomato"));
+    ft_list_push_front(&lst, strdup("other"));
+
+    ft_list_remove_if(&lst, "banana", strcmp, free);
+    printf("(list[size 7], remove('banana')) : other, tomato, ZEBRA, milk, milk, banana, banana\n");
+    print_list(&lst);
+    
+    double_free_list(&lst);
+}
+
+void waitUserKeyPress() {
+    printf("\033[999;1HPress enter to see next");
+    getchar();
+    printf("\033[2J\033[H");
 }
 
 int main(void)
 {
+    printf("\033[2J\033[H");
+
     printHeader("Test ft_atoi_base");
     test_atoi_base();
+
+    waitUserKeyPress();
 
     printHeader("Test ft_list_push_front");
     test_push_front();
 
+    waitUserKeyPress();
+
     printHeader("Test ft_list_size");
     test_lst_size();
 
+    waitUserKeyPress();
+
     printHeader("Test ft_list_sort");
     test_lst_sort();
+
+    waitUserKeyPress();
     
     printHeader("Test ft_list_remove_if");
     test_lst_remove();
--- test.c
+++ test.c
@@ -7,6 +7,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define RESET "\033[0m"
+#define TITLE "\033[3;32m"
+#define SUBTITLE "\033[3;35m"
+
 // Déclaration des fonctions ASM
 extern size_t ft_strlen(const char *s);
 extern char *ft_strcpy(char *dest, const char *src);
@@ -26,7 +30,7 @@ void printHeader(const char *str)
     {
         printf(" ");
     }
-    printf("\033[3;32m%s\033[0m",str);
+    printf(TITLE"%s"RESET,str);
     for(int i=0; i < ((len / 2) + len % 2); i++)
     {
         printf(" ");
@@ -42,13 +46,26 @@ void errorAlloc(void)
     exit(1);
 }
 
+// ##### Tests #####
 void strlen_test(void)
 {
-    char *test = "Hey, its a msg !";
-    int len = strlen(test);
-    int ft_len = ft_strlen(test);
+    int len = -1;
+    int ft_len = -1;
 
-    printf("The string is '%s' \nstrlen : %d \nft_strlen : %d\n", test, len, ft_len);
+    printf("---------- "SUBTITLE"Test with len 0"RESET" ----------\n");
+    ft_len = ft_strlen("");
+    len = strlen("");
+    printf("('') -> strlen : %d, ft_strlen : %d\n", len, ft_len);
+
+    printf("\n---------- "SUBTITLE"Test with len 3"RESET" ----------\n");
+    ft_len = ft_strlen("Yep");
+    len = strlen("Yep");
+    printf("('Yep') -> strlen : %d, ft_strlen : %d\n", len, ft_len);
+
+    printf("\n---------- "SUBTITLE"Test with len 16 (Special)"RESET" ----------\n");
+    ft_len = ft_strlen("Jump line \n Here");
+    len = strlen("Jump line \n Here");
+    printf("('Jump line \\n Here') -> strlen : %d, ft_strlen : %d\n", len, ft_len);
 }
 
 void strcpy_test(void)
@@ -58,71 +75,100 @@ void strcpy_test(void)
         errorAlloc();
     bzero(dest, 8);
 
-    dest = strcpy(dest, "coucou!");
-    printf("strcpy 'coucou!' -> '%s'\n", dest);
+    printf("---------- "SUBTITLE"Test with len 0"RESET" ----------\n");
+    printf("(dest[malloc 8], '') :\n");
+
+    dest = strcpy(dest, "");
+    printf("strcpy : '%s',\n", dest);
     bzero(dest, 8);
-    printf("clean dest -> '%s'\n", dest);
+
+    dest = ft_strcpy(dest, "");
+    printf("ft_strcpy : '%s'\n", dest);
+    bzero(dest, 8);
+
+    printf("\n---------- "SUBTITLE"Test with len 3"RESET" ----------\n");
+    printf("(dest[malloc 8], 'hey!') :\n");
+    
+    dest = strcpy(dest, "hey!");
+    printf("strcpy : '%s',\n", dest);
+    bzero(dest, 8);
+
+    dest = ft_strcpy(dest, "hey!");
+    printf("ft_strcpy : '%s'\n", dest);
+    bzero(dest, 8);
+
+    printf("\n---------- "SUBTITLE"Test with len 7 (size+'\\0')"RESET" ----------\n");
+    printf("(dest[malloc 8], 'coucou!') :\n");
+    
+    dest = strcpy(dest, "coucou!");
+    printf("strcpy : '%s',\n", dest);
+    bzero(dest, 8);
+
     dest = ft_strcpy(dest, "coucou!");
-    printf("ft_strcpy 'coucou!' -> '%s'\n", dest);
+    printf("ft_strcpy : '%s'\n", dest);
+    bzero(dest, 8);
 
     free(dest);
 }
 
 void strcmp_test(void)
 {
-    char *s1 = "coucou1";
-    char *s2 = "coucou1";
-    char *s3 = "coutou1";
-    char *s4 = "coucou";
+    char *s1 = "same";
+    char *s2 = "same";
+    char *s3 = "diff";
+    char *s4 = "samegreater";
     int cmp = 0;
     int ft_cmp = 0;
 
+    printf("---------- "SUBTITLE"Test equal string"RESET" ----------\n");
     cmp = strcmp(s1, s2);
     ft_cmp = ft_strcmp(s1, s2);
-    printf("Test equal string :\n");
-    printf("strcmp '%s' - '%s' : %d\n", s1, s2, cmp);
-    printf("ft_strcmp '%s' - '%s' : %d\n\n", s1, s2, ft_cmp);
+    printf("('%s', '%s') :\n", s1, s2);
+    printf("strcmp : %d\n", cmp);
+    printf("ft_strcmp :%d\n", ft_cmp);
 
+    printf("\n---------- "SUBTITLE"Test not equal string"RESET" ----------\n");
     cmp = strcmp(s1, s3);
     ft_cmp = ft_strcmp(s1, s3);
-    printf("Test not equal string :\n");
-    printf("strcmp '%s' - '%s' : %d\n", s1, s3, cmp);
-    printf("ft_strcmp '%s' - '%s' : %d\n\n", s1, s3, ft_cmp);
+    printf("('%s', '%s') :\n", s1, s3);
+    printf("strcmp : %d\n", cmp);
+    printf("ft_strcmp : %d\n", ft_cmp);
 
+    printf("\n---------- "SUBTITLE"Test second string greater"RESET" ----------\n");
     cmp = strcmp(s1, s4);
     ft_cmp = ft_strcmp(s1, s4);
-    printf("Test s1 > s2 :\n");
-    printf("strcmp '%s' - '%s' : %d\n", s1, s4, cmp);
-    printf("ft_strcmp '%s' - '%s' : %d\n\n", s1, s4, ft_cmp);
+    printf("('%s', '%s') :\n", s1, s4);
+    printf("strcmp : %d\n", cmp);
+    printf("ft_strcmp : %d\n", ft_cmp);
 
+    printf("\n---------- "SUBTITLE"Test first string greater"RESET" ----------\n");
     cmp = strcmp(s4, s1);
     ft_cmp = ft_strcmp(s4, s1);
-    printf("Test s1 < s2 :\n");
-    printf("strcmp '%s' - '%s' : %d\n", s4, s1, cmp);
-    printf("ft_strcmp '%s' - '%s' : %d\n", s4, s1, ft_cmp);
+    printf("('%s', '%s') :\n", s4, s1);
+    printf("strcmp : %d\n", cmp);
+    printf("ft_strcmp : %d\n", ft_cmp);
 }
 
 void write_test(void)
 {
     int back = 0;
 
-    printf("Test with write on fd 1\n");
+    printf("---------- "SUBTITLE"Test on fd 1"RESET" ----------\n");
+    printf("(1, 'this is a test\\n', 15) :\n");
     back = write(1, "this is a test\n", 15);
-    printf("back of write is : %d\n", back);
-
-    printf("\nTest with ft_write on fd 1\n");
+    printf("write : %d\n\n", back);
     back = ft_write(1, "this is a test\n", 15);
-    printf("back of ft_write is : %d\n", back);
+    printf("ft_write : %d\n", back);
 
-    printf("\nBad write on fd -1\n");
+    printf("\n---------- "SUBTITLE"Test bad fd -1"RESET" ----------\n");
+
+    printf("(-1, 'this is a test\\n', 15) :\n");
     back = write(-1, "this is a test\n", 15);
-    printf("back of write is : %d and errno : %d\n", back, errno);
-
+    printf("write : %d, errno : %d\n", back, errno);
     errno = 0;
-
-    printf("\nBad ft_write on fd -1\n");
+    
     back = ft_write(-1, "this is a test\n", 15);
-    printf("back of ft_write is : %d and errno : %d\n", back, errno);
+    printf("ft_write : %d, errno : %d\n", back, errno);
 }
 
 void read_test(void)
@@ -131,62 +177,106 @@ void read_test(void)
     char buff[4];
     int back = 0;
 
-    printf("try with read on read_test file on fd %d\n", fd);
+    printf("---------- "SUBTITLE"Test read file"RESET" ----------\n");
+
+    printf("(%d, buff, 3) :\n", fd);
+    printf("read :\n");
     while ((back = read(fd, buff, 3)) > 0)
     {
         buff[back] = 0;
-        printf("buffer of read '%s', nb byte readed : %d\n", buff, back);
+        printf("buffer : '%s', byte readed : %d\n", buff, back);
     }
     close(fd);
     fd = open("./read_test", O_CREAT);
 
-    printf("\ntry with ft_read on read_test file on fd %d\n", fd);
+
+    printf("\nft_read :\n");
     while ((back = ft_read(fd, buff, 3)) > 0)
     {
         buff[back] = 0;
-        printf("buffer of ft_read '%s', nb byte readed : %d\n", buff, back);
+        printf("buffer : '%s', byte readed : %d\n", buff, back);
     }
     
     close(fd);
 
+    printf("\n---------- "SUBTITLE"Test bad fd"RESET" ----------\n");
+
+    printf("(-1, buff, 3) :\n");
     back = read(-1, buff, 3);
-    printf("\nBad fd (-1) on read : %d, errno : %d\n", back, errno);
+    printf("read : %d, errno : %d\n", back, errno);
     errno = 0;
     back = ft_read(-1, buff, 3);
-    printf("Bad fd (-1) on ft_read : %d, errno : %d\n", back, errno);
+    printf("ft_read : %d, errno : %d\n", back, errno);
 }
 
 void strdup_test(void)
 {
     char *duplicated;
 
+    printf("---------- "SUBTITLE"Test empty copy"RESET" ----------\n");
+    printf("('') :\n");
+    duplicated = strdup("");
+    if (!duplicated)
+        errorAlloc();
+    printf("strdup : '%s'\n", duplicated);
+    free(duplicated);
+    duplicated = NULL;
+
+    duplicated = ft_strdup("");
+    if (!duplicated)
+        errorAlloc();
+    printf("ft_strdup : '%s'\n", duplicated);
+    free(duplicated);
+    duplicated = NULL;
+
+
+    printf("\n---------- "SUBTITLE"Test copy"RESET" ----------\n");
+    printf("('Hey, this is my msg') :\n");
     duplicated = strdup("Hey, this is my msg");
     if (!duplicated)
         errorAlloc();
-    printf("try duplicate with strdup : %s\n", duplicated);
+    printf("strdup : '%s'\n", duplicated);
     free(duplicated);
     duplicated = NULL;
 
     duplicated = ft_strdup("Hey, this is my msg");
     if (!duplicated)
         errorAlloc();
-    printf("try duplicate with ft_strdup : %s\n", duplicated);
+    printf("ft_strdup : '%s'\n", duplicated);
     free(duplicated);
     duplicated = NULL;
 }
 
+void waitUserKeyPress() {
+    printf("\033[999;1HPress enter to see next");
+    getchar();
+    printf("\033[2J\033[H");
+}
+
 int main(void)
 {
+    printf("\033[2J\033[H");
+
     printHeader("Test of ft_strlen");
 	strlen_test();
+    waitUserKeyPress();
+
     printHeader("Test of ft_strcpy");
     strcpy_test();
+    waitUserKeyPress();
+
     printHeader("Test of ft_strcmp");
     strcmp_test();
+    waitUserKeyPress();
+
     printHeader("Test of ft_write");
     write_test();
+    waitUserKeyPress();
+
     printHeader("Test of ft_read");
     read_test();
+    waitUserKeyPress();
+
     printHeader("Test of ft_strdup");
     strdup_test();
     return (0);
