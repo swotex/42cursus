1e72dbfa585dde46ea2d8a060a5d0a7002528f02
swotex <swotex21@gmail.com>
2025-04-17 19:14:11 +0200

    Remove comment

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,19 +1,3 @@
-# curl -sfL https://get.k3s.io | sh -s - agent --token-file /vagrant_data/node-token --server 192.168.56.110:6443
-
-#while [ ! -f /vagrant_data/node-token ]; do
-#    echo "En attente du token depuis le serveur..."
-#    sleep 2
-#done
-
-# Lire le token
-#TOKEN=$(cat /vagrant_data/node-token)
-
-# Lancer K3s en mode agent
-#curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
-
-
-
-
 token_file="/vagrant_data/node-token"
 
 while [ ! -f $token_file ]; do
