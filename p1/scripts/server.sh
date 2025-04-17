ce5fe8115103552601a3a3dbe4fddd4c2bc670de
swotex <swotex21@gmail.com>
2025-04-17 19:06:29 +0200

    Remove the double call to daemon K3S. Share cluster token between server/agent. Connect Agent to Server

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,2 +1,9 @@
-curl -sfL https://get.k3s.io | sh -
-k3s server &
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
+
+token_file="/var/lib/rancher/k3s/server/node-token"
+
+while [ ! -f "$token_file" ]; do
+    echo "Waiting for cluster token creation"
+    sleep 1
+done
+cat $token_file > /vagrant_data/node-token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,2 +1,28 @@
-curl -sfL https://get.k3s.io | sh -
-k3s agent &
+# curl -sfL https://get.k3s.io | sh -s - agent --token-file /vagrant_data/node-token --server 192.168.56.110:6443
+
+#while [ ! -f /vagrant_data/node-token ]; do
+#    echo "En attente du token depuis le serveur..."
+#    sleep 2
+#done
+
+# Lire le token
+#TOKEN=$(cat /vagrant_data/node-token)
+
+# Lancer K3s en mode agent
+#curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
+
+
+
+
+token_file="/vagrant_data/node-token"
+
+while [ ! -f $token_file ]; do
+    echo "Waiting token from server..."
+    sleep 2
+done
+
+curl -sfL https://get.k3s.io | \
+K3S_TOKEN_FILE=$token_file \
+K3S_URL=https://192.168.56.110:6443 \
+INSTALL_K3S_EXEC="--node-ip=192.168.56.111" \
+sh -
